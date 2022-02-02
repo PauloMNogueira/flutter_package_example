0625958c319490867917ae01cc48ca132d1957eb
PauloMNogueira <paulo.nogueira@jcpm.com.br>
2022-02-02 10:17:52 -0300

    Initial Commit

---
+++ lib/teste_package.dart
@@ -0,0 +1,35 @@
+library teste_package;
+
+import 'package:flutter/material.dart';
+
+class CustomAlertBox {
+  /// Bu şekilde döküman yorumları oluşturabilirsiniz kullanan kişiler için faydalı olur.
+  static Future showCustomAlertBox({
+    required BuildContext context,
+    required Widget willDisplayWidget,
+  }) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: const Text('close alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
+}
