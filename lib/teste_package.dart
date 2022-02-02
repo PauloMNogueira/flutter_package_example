714e2b76544c1490bed902995f98d46cf9c64dac
PauloMNogueira <paulo.nogueira@jcpm.com.br>
2022-02-02 10:29:48 -0300

    teste

--- lib/teste_package.dart
+++ lib/teste_package.dart
@@ -2,34 +2,17 @@ library teste_package;
 
 import 'package:flutter/material.dart';
 
-class CustomAlertBox {
-  /// Bu şekilde döküman yorumları oluşturabilirsiniz kullanan kişiler için faydalı olur.
-  static Future showCustomAlertBox({
-    required BuildContext context,
-    required Widget willDisplayWidget,
-  }) {
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(15)),
-            ),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                willDisplayWidget,
-                MaterialButton(
-                  color: Colors.white30,
-                  child: const Text('close alert'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                )
-              ],
-            ),
-            elevation: 10,
-          );
-        });
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+class TestePackageWidget extends StatelessWidget {
+  const TestePackageWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Olá, sou um modulo externo");
   }
 }
